25d898c95eb654b239878d7d0f41c75ee19e8e47
swatcam1@gmail.com <>
2019-02-01 09:25:10 -0500

    This is my first commit

---
+++ src/github/name/GitHubName.java
@@ -0,0 +1,11 @@
+package github.name;
+
+public class GitHubName {
+    public static void main(String[] args) {
+        Young();
+    }
+    
+    public static void Young(){
+        System.out.println("Cameron Young");
+    }
+}
